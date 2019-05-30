a4d3b328812f6abf19fd7245b7fee53b222f803e
mokhno <37487922+mokhno@users.noreply.github.com>
2019-05-30 21:52:05 +0300

    -

--- app/src/main/java/com/example/chulakovtest/RecyclerFragment.kt
+++ app/src/main/java/com/example/chulakovtest/RecyclerFragment.kt
@@ -2,9 +2,9 @@ package com.example.chulakovtest
 
 import android.content.Context
 import android.os.Bundle
-import android.support.v4.app.Fragment
-import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -30,8 +30,8 @@ class RecyclerFragment : Fragment() {
         }
     }
 
-    override fun onAttach(context: Context?) {
-        super.onAttach(context)
+    override fun onAttach(context: Context) {
+        super.onAttach(context!!)
         if (context is UsersAdapter.OnItemClickListener) {
             mListener = context
         }
