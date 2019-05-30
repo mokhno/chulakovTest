66d6cc35bd496c8cef9e7b0ce00f9ad65973be0c
mokhno <37487922+mokhno@users.noreply.github.com>
2019-05-30 21:03:37 +0300

    -

---
+++ app/src/main/java/com/example/chulakovtest/ProfileFragment.kt
@@ -0,0 +1,91 @@
+package com.example.chulakovtest
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+//import android.os.Build.USER
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import com.example.chulakovtest.Model.Profile
+import com.example.chulakovtest.Model.User
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.fr_profile.*
+import kotlinx.android.synthetic.main.res_user.*
+import retrofit2.Call
+import retrofit2.Response
+
+class ProfileFragment : Fragment() {
+
+    private lateinit var user: User
+    lateinit var profile: Profile
+
+    companion object {
+        fun newInstance(user: User): ProfileFragment {
+            var prFragment = ProfileFragment()
+
+            var args = Bundle()
+            args.putParcelable("USER", user)
+            prFragment.arguments = args
+            return prFragment
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d("tag", "args notnull")
+        user = arguments!!.getParcelable("USER")
+        getProfile(user)
+        Log.d("tag", "massage" + user.id)
+
+
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.fr_profile, container, false)
+
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+    }
+
+    fun getProfile(user: User) {
+        ApiGit.apiService.getProfile(user.login).enqueue(object : retrofit2.Callback<Profile> {
+            override fun onFailure(call: Call<Profile>, t: Throwable) {
+
+            }
+
+            override fun onResponse(call: Call<Profile>, response: Response<Profile>) {
+
+                profile = response.body()!!
+
+
+//                var avatar: ImageView = view.findViewById(R.id.imageViewAvatar)
+
+                Picasso.get().load(Uri.parse(profile.avatar_url)).into(imageViewAvatar)
+
+                loginTv.text = profile.login
+                tv_html.text = "Ссылка на профиль"
+                tv_html.setOnClickListener {
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(user.html_url))
+                    startActivity(intent)
+                }
+                tv_bio.text = profile.bio
+            }
+
+
+        })
+
+    }
+
+}
